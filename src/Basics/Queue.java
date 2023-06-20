d835bb0b43ce57f664b8e7f186a26933b7cb865d
Dhushyanth-Huliyurdurga-Manjunath <125176193+Dhushyanth-Huliyurdurga-Manjunath@users.noreply.github.com>
2023-06-20 20:54:30 +0000

    Linear Queue Implementation

---
+++ src/Basics/Queue.java
@@ -0,0 +1,68 @@
+import  java.util.EmptyStackException;
+
+public class Queue {
+    private int maxSize;
+    private int[] queueArray;
+    private int front;
+    private int rear;
+    private int currentSize;
+
+    public Queue(int size) {
+        maxSize = size;
+        queueArray = new int[maxSize];
+        front = 0;
+        rear = -1;
+        currentSize = 0;
+    }
+
+    public boolean isEmpty(){
+        return currentSize == 0;
+    }
+
+    public boolean isFull(){
+        return currentSize == maxSize;
+    }
+
+    public void enqueue(int value){
+        if(isFull()){
+            System.out.println("Queue is full!.");
+            return;
+        }
+        rear += 1;
+        currentSize += 1;
+        queueArray[rear] = value;
+    }
+
+    public int dequeue(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+            // return 0;
+        }
+        int dequeuedValue = queueArray[front];
+        if(front == rear){
+            front = rear = -1;
+        }
+        front += 1;
+        currentSize -= 1;
+        return dequeuedValue;
+    }
+
+    public int size(){
+        return currentSize;
+    }
+
+    public static void main(String[] args){
+        Queue queue = new Queue(4);
+
+        queue.enqueue(234);
+        queue.enqueue(184);
+        queue.enqueue(693);
+        queue.enqueue(952);
+
+        System.out.println("Size: "+queue.size());
+        
+        while(!queue.isEmpty()){
+            System.out.println(queue.dequeue());
+        }
+    }
+}
